fc4e0c54138c466b42f6c95ed344c98c26cb7c82
hmzshin <hamzasahin@outlook.com>
2023-11-14 20:29:35 +0300

    fix bg-red condition

--- src/Task.js
+++ src/Task.js
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from "react";
 import { intlFormatDistance, differenceInDays } from "date-fns";
 
 const Task = ({ taskObj, onComplete }) => {
-  const deadline = new Date(taskObj.deadline);
-  deadline.setHours(23, 59, 59, 999);
+  const deadline = new Date(taskObj.deadline).setHours(23, 59, 59, 999);
 
   const daysLeft = differenceInDays(deadline, Date.now());
   console.log("days left:", daysLeft);
-  console.log("3 den az mı ", daysLeft <= 3);
+  console.log("less then 3?", daysLeft <= 3);
   console.log("***********render bitti*************");
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className={daysLeft <= 3 ? "deadline bg-red-400" : "deadline"}>
-        son teslim:
-        <span>{intlFormatDistance(deadline, Date.now())}</span>
+      <div className="deadline">
+        son teslim:{" "}
+        <span className={daysLeft <= 3 ? "bg-red-400" : "bg-indigo-200 "}>
+          {intlFormatDistance(deadline, Date.now())}
+        </span>
       </div>
       <p>{taskObj.description}</p>
       <div>
